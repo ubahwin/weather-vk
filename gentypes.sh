2399770d24d32fe369da0b3d7eb0010efc24ba5a
Ivan Vdovin <ivan03_vdovin@mail.ru>
2024-03-21 12:13:45 +0300

    rm(responses): useless JSONEncodable

--- gentypes.sh
+++ gentypes.sh
@@ -6,6 +6,18 @@ if [ -d "tmp/OpenAPIClient/Classes/OpenAPIs/Models" ]; then
     cp -r tmp/OpenAPIClient/Classes/OpenAPIs/Models/. WeatherVK/Network/DTO/Response/
 fi
 
+if [ -d "WeatherVK/Network/DTO/Response" ]; then
+    cd WeatherVK/Network/DTO/Response/
+
+    for file in *.swift; do
+        sed -i '' 's/, JSONEncodable//g' "$file"
+    done
+
+    cd ../../../..
+fi
+
+ls
+
 rm -rf tmp
 
 echo "Done."

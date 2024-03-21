1d1cffae27c315ff64ff2c3aad9d2d3d525123c1
Ivan Vdovin <ivan03_vdovin@mail.ru>
2024-03-21 12:16:20 +0300

    feat forecast

--- WeatherVK/Network/DTO/Mapper.swift
+++ WeatherVK/Network/DTO/Mapper.swift
@@ -1,10 +1,21 @@
 struct Mapper {
-    static func currentWeatherToModel(_ curWeatherResponse: CurrentWeatherResponse) -> Weather {
+    static func currentWeatherToModel(_ curWeatherResponse: GetWeatherData200Response) -> Weather {
         Weather(
             temperature: Int(curWeatherResponse.main?.temp ?? 0),
             windSpeed: curWeatherResponse.wind?.speed ?? 0,
             windDirectDegrees: Double(curWeatherResponse.wind?.deg ?? 0),
             clouds: curWeatherResponse.clouds?.all ?? 0
         )
+    }
+
+    static func currentForecastToModel(_ curForecastResponse: GetWeatherForecast200Response) -> Forecast {
+        return curForecastResponse.list?.compactMap { forecastResponse in
+            return Weather(
+                temperature: Int(forecastResponse.main?.temp ?? 0),
+                windSpeed: forecastResponse.wind?.speed ?? 0,
+                windDirectDegrees: Double(forecastResponse.wind?.deg ?? 0),
+                clouds: forecastResponse.clouds?.all ?? 0
+            )
+        } ?? []
     }
 }

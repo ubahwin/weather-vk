8e4bc2aeccf4222fa0512ec2ae49febdf1aa42c1
Ivan Vdovin <ivan03_vdovin@mail.ru>
2024-03-22 15:43:15 +0300

    feat: tests data processing

--- WeatherVKTests/WeatherVKTests.swift
+++ WeatherVKTests/WeatherVKTests.swift
@@ -1,35 +1,195 @@
-//
-//  WeatherVKTests.swift
-//  WeatherVKTests
-//
-//  Created by Иван Вдовин on 21.03.2024.
-//
-
 import XCTest
+@testable import WeatherVK
 
 final class WeatherVKTests: XCTestCase {
+    private let forecastFormat = ForecastFormatter()
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_forecastFormat_calcDayWeek_fromTimeStamp1() {
+        let timeInt: TimeInterval = 1711022400
+        XCTAssertEqual(
+            forecastFormat.calcDayWeek(utc: timeInt),
+            .thursday
+        )
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func test_forecastFormat_calcDayWeek_fromTimeStamp2() {
+        let timeInt: TimeInterval = 1711054800
+        XCTAssertEqual(
+            forecastFormat.calcDayWeek(utc: timeInt),
+            .friday
+        )
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_forecastFormat_calcDayWeek_fromTimeStamp3() {
+        let timeInt: TimeInterval = 1711324800
+        XCTAssertEqual(
+            forecastFormat.calcDayWeek(utc: timeInt),
+            .monday
+        )
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_forecastFormat_clean_empty() {
+        let dirtyForecast = DirtyForecast(weatherList: [])
+
+        XCTAssertEqual(
+            forecastFormat.clean(dirtyForecast: dirtyForecast),
+            []
+        )
     }
 
+    func test_forecastFormat_clean_thursdayAndMonday() {
+        let timeIntMonday: TimeInterval = 1711324800
+        let timeIntThursday: TimeInterval = 1711022400
+
+        let weatherList = [
+            DirtyWeather(
+                timeStamb: timeIntMonday,
+                minTemp: 1,
+                maxTemp: 1,
+                windSpeed: 1,
+                clouds: 1
+            ),
+            DirtyWeather(
+                timeStamb: timeIntMonday,
+                minTemp: 1,
+                maxTemp: 1,
+                windSpeed: 1,
+                clouds: 1
+            ),
+            DirtyWeather(
+                timeStamb: timeIntMonday,
+                minTemp: 1,
+                maxTemp: 1,
+                windSpeed: 1,
+                clouds: 1
+            ),
+            DirtyWeather(
+                timeStamb: timeIntMonday,
+                minTemp: 1,
+                maxTemp: 1,
+                windSpeed: 1,
+                clouds: 1
+            ),
+            DirtyWeather(
+                timeStamb: timeIntThursday,
+                minTemp: 2,
+                maxTemp: 2,
+                windSpeed: 2,
+                clouds: 2
+            ),
+            DirtyWeather(
+                timeStamb: timeIntThursday,
+                minTemp: 2,
+                maxTemp: 2,
+                windSpeed: 2,
+                clouds: 2
+            )
+        ]
+        let dirtyForecast = DirtyForecast(weatherList: weatherList)
+
+        XCTAssertEqual(
+            forecastFormat.clean(dirtyForecast: dirtyForecast),
+            [
+                Forecast(
+                    dayweek: .monday,
+                    wearher: Weather(
+                        temperature: 0,
+                        minTemp: 1,
+                        maxTemp: 1,
+                        windSpeed: 1,
+                        windDirectDegrees: 0,
+                        clouds: 1
+                    )
+                ),
+                Forecast(
+                    dayweek: .thursday,
+                    wearher: Weather(
+                        temperature: 0,
+                        minTemp: 2,
+                        maxTemp: 2,
+                        windSpeed: 2,
+                        windDirectDegrees: 0,
+                        clouds: 2
+                    )
+                )
+            ]
+        )
+    }
+
+    let createAverageWeatherList1 = [
+        DirtyWeather(
+            timeStamb: 1711324800,
+            minTemp: 7,
+            maxTemp: 4,
+            windSpeed: 1,
+            clouds: 0
+        ),
+        DirtyWeather(
+            timeStamb: 1711324800,
+            minTemp: 2,
+            maxTemp: 9,
+            windSpeed: 2,
+            clouds: 0
+        ),
+        DirtyWeather(
+            timeStamb: 1711324800,
+            minTemp: 3,
+            maxTemp: 2,
+            windSpeed: 3,
+            clouds: 0
+        ),
+        DirtyWeather(
+            timeStamb: 1711324800,
+            minTemp: -4,
+            maxTemp: -1,
+            windSpeed: 4,
+            clouds: 100
+        ),
+        DirtyWeather(
+            timeStamb: 1711324800,
+            minTemp: 4,
+            maxTemp: 10,
+            windSpeed: 5,
+            clouds: 100
+        ),
+        DirtyWeather(
+            timeStamb: 1711324800,
+            minTemp: 5,
+            maxTemp: 1,
+            windSpeed: 9,
+            clouds: 100
+        )
+    ]
+
+    func test_forecastFormat_createAverageWeather_1() {
+        XCTAssertEqual(
+            forecastFormat.createAverageWeather(list: createAverageWeatherList1),
+            Weather(
+                temperature: 0,
+                minTemp: -4,
+                maxTemp: 10,
+                windSpeed: 4,
+                windDirectDegrees: 0,
+                clouds: 50
+            )
+        )
+    }
+
+}
+
+extension Forecast: Equatable {
+    public static func == (lhs: WeatherVK.Forecast, rhs: WeatherVK.Forecast) -> Bool {
+        lhs.dayweek == rhs.dayweek && lhs.wearher == rhs.wearher
+    }
+}
+
+extension Weather: Equatable {
+    public static func == (lhs: Weather, rhs: Weather) -> Bool {
+        lhs.temperature == rhs.temperature &&
+        lhs.minTemp == rhs.minTemp &&
+        lhs.maxTemp == rhs.maxTemp &&
+        lhs.windSpeed == rhs.windSpeed &&
+        lhs.windDirectDegrees == rhs.windDirectDegrees &&
+        lhs.clouds == rhs.clouds
+    }
 }

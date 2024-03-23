6a8ab7051111e1f1b75b3d3b9cf314903a74fac0
Ivan Vdovin <ivan03_vdovin@mail.ru>
2024-03-23 20:47:13 +0300

    feat(main-view): cities picker

---
+++ WeatherVK/Utility/KeyboardHeight.swift
@@ -0,0 +1,21 @@
+import Combine
+import SwiftUI
+
+extension Publishers {
+    static var keyboardHeight: AnyPublisher<CGFloat, Never> {
+        let willShow = NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)
+            .map { $0.keyboardHeight }
+
+        let willHide = NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
+            .map { _ in CGFloat(0) }
+
+        return MergeMany(willShow, willHide)
+            .eraseToAnyPublisher()
+    }
+}
+
+extension Notification {
+    var keyboardHeight: CGFloat {
+        (userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect)?.height ?? 0
+    }
+}

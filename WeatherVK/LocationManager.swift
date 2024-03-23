e340df01be943fdfa902b93fdcb53598bc10bc1a
Ivan Vdovin <ivan03_vdovin@mail.ru>
2024-03-23 20:45:40 +0300

    feat(location-manager): load cities

--- WeatherVK/LocationManager.swift
+++ WeatherVK/LocationManager.swift
@@ -1,19 +1,27 @@
 import CoreLocation
 import Combine
+import MapKit
 
 protocol ILocationManager {
-    var userLocation: CurrentValueSubject<CLLocationCoordinate2D, Never> { get }
+    func loadCurrentCity() -> AnyPublisher<City, Never>
+    func loadCities(from search: String) -> AnyPublisher<[City], Never>
+
     var phoneRotateDegrees: CurrentValueSubject<Double, Never> { get }
-    var cityName: CurrentValueSubject<String, Never> { get }
 }
 
-class LocationManager: NSObject, CLLocationManagerDelegate, ILocationManager {
-    let userLocation = CurrentValueSubject<CLLocationCoordinate2D, Never>(CLLocationCoordinate2D())
-    let phoneRotateDegrees = CurrentValueSubject<Double, Never>(0)
+final class LocationManager: NSObject, CLLocationManagerDelegate, ILocationManager {
     let cityName = CurrentValueSubject<String, Never>("")
+    let userLocation = CurrentValueSubject<CLLocationCoordinate2D, Never>(CLLocationCoordinate2D())
+
+    let phoneRotateDegrees = CurrentValueSubject<Double, Never>(0)
 
     private let locationManager = CLLocationManager()
     private let geocoder = CLGeocoder()
+
+    private let searchRegion = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(),
+        span: MKCoordinateSpan(latitudeDelta: 180, longitudeDelta: 360)
+    )
 
     override init() {
         super.init()
@@ -23,19 +31,48 @@ class LocationManager: NSObject, CLLocationManagerDelegate, ILocationManager {
         locationManager.startUpdatingHeading()
     }
 
-    private func getCityName(from coordinates: CLLocationCoordinate2D) {
-        let location = CLLocation(latitude: coordinates.latitude, longitude: coordinates.longitude)
-
-        geocoder.reverseGeocodeLocation(location) { (placemarks, _) in
-            guard
-                let placemark = placemarks?.first,
-                let city = placemark.locality
-            else {
-                return
+    func loadCurrentCity() -> AnyPublisher<City, Never> {
+        cityName.zip(userLocation)
+            .map { name, location in
+                return City(name: name, coordinates: location)
             }
+            .eraseToAnyPublisher()
+    }
 
-            self.cityName.send(city)
+    func loadCities(from search: String) -> AnyPublisher<[City], Never> {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = search
+        request.region = searchRegion
+
+        return Future<[City], Never> { promise in
+            MKLocalSearch(request: request).start { response, error in
+                guard let response = response else {
+                    if let error = error {
+                        Log.error(error.localizedDescription)
+                    }
+
+                    promise(.success([]))
+                    return
+                }
+
+                print(response.mapItems)
+
+                let cities = response.mapItems.compactMap { mapItem -> City? in
+                    guard
+                        let cityName = mapItem.name,
+                        mapItem.placemark.administrativeArea != nil,
+                        mapItem.pointOfInterestCategory == nil
+                    else {
+                        return nil
+                    }
+
+                    return City(name: cityName, coordinates: mapItem.placemark.coordinate)
+                }
+
+                promise(.success(cities))
+            }
         }
+        .eraseToAnyPublisher()
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -48,6 +85,18 @@ class LocationManager: NSObject, CLLocationManagerDelegate, ILocationManager {
         getCityName(from: coordinate)
 
         locationManager.stopUpdatingLocation()
+    }
+
+    private func getCityName(from coordinates: CLLocationCoordinate2D) {
+        let location = CLLocation(latitude: coordinates.latitude, longitude: coordinates.longitude)
+
+        geocoder.reverseGeocodeLocation(location) { (placemarks, _) in
+            guard let city = placemarks?.first?.locality else {
+                return
+            }
+
+            self.cityName.send(city)
+        }
     }
 
     func locationManager(
@@ -64,6 +113,14 @@ class LocationManager: NSObject, CLLocationManagerDelegate, ILocationManager {
 }
 
 class StubLocationManager: ILocationManager {
+    func loadCities(from search: String) -> AnyPublisher<[City], Never> {
+        Just<[City]>([]).eraseToAnyPublisher()
+    }
+
+    func loadCurrentCity() -> AnyPublisher<City, Never> {
+        Just<City>(.stub).eraseToAnyPublisher()
+    }
+
     let cityName = CurrentValueSubject<String, Never>("")
     let phoneRotateDegrees = CurrentValueSubject<Double, Never>(0)
     let userLocation = CurrentValueSubject<CLLocationCoordinate2D, Never>(CLLocationCoordinate2D())

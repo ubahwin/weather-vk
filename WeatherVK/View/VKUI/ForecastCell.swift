7f4439f36c8e67313b9745e0654550c146662b54
Ivan Vdovin <ivan03_vdovin@mail.ru>
2024-03-25 02:12:59 +0300

    chore(view): simplify

---
+++ WeatherVK/View/VKUI/ForecastCell.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+struct ForecastCell: View {
+    var forecast: Forecast
+
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(forecast.date.dayOfWeek.titleVKUI)
+                    .foregroundStyle(forecast.date.dayOfWeek.isWeekend ? Color(hex: 0xE64646) : .black)
+                Text(forecast.date.title)
+                    .foregroundStyle(.gray)
+                    .font(.footnote)
+            }
+            Spacer()
+            Text("\(forecast.weather.maxTemp)°")
+                .frame(width: 60)
+                .bold()
+            Text("\(forecast.weather.minTemp)°")
+                .bold()
+                .foregroundStyle(Color(hex: 0x76787A))
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 5)
+    }
+}

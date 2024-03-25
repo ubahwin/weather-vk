83e829e62a657715940d2de81395d93b02ccd370
Ivan Vdovin <ivan03_vdovin@mail.ru>
2024-03-25 21:46:40 +0300

    fix(forecast-cell): element align

--- WeatherVK/View/VKUI_SwiftUI/Component/ForecastCell.swift
+++ WeatherVK/View/VKUI_SwiftUI/Component/ForecastCell.swift
@@ -13,15 +13,34 @@ struct ForecastCell: View {
                     .font(.footnote)
             }
             Spacer()
+            Text(forecast.weather.type.image)
+                .frame(width: 50, alignment: .trailing)
             Text("\(forecast.weather.maxTemp)°")
-                .frame(width: 60)
+                .frame(width: 50, alignment: .trailing)
                 .bold()
             Text("\(forecast.weather.minTemp)°")
-                .padding(.leading)
+                .frame(width: 50, alignment: .trailing)
                 .bold()
                 .foregroundStyle(Color(hex: 0x76787A))
         }
         .padding(.horizontal)
         .padding(.vertical, 5)
     }
+}
+
+#Preview {
+    ForecastCell(forecast: Forecast(
+        date: .now,
+        weather: Weather(
+            type: .clouds,
+            temperature: 1,
+            minTemp: -11,
+            maxTemp: 12,
+            windSpeed: 1,
+            windDirectDegrees: 1,
+            clouds: 1,
+            pressure: 1,
+            visibility: 1
+        ))
+    )
 }

0e88c0722422181f075620219adf3427c8009dc1
Ivan Vdovin <ivan03_vdovin@mail.ru>
2024-03-25 19:29:41 +0300

    feat(sui-view): emoji panel

--- WeatherVK/Model/WeatherType.swift
+++ WeatherVK/Model/WeatherType.swift
@@ -1,27 +1,21 @@
 import SwiftUI
 
 enum WeatherType: String, Codable {
-    case clearSky = "clear sky"
-    case fewClouds = "few clouds"
-    case scatteredClouds = "scattered clouds"
-    case brokenClouds = "broken clouds"
-    case showerRain = "shower rain"
-    case rain
-    case thunderstorm
-    case snow
-    case mist
+    case clearSky = "Clear sky"
+    case clouds = "Clouds"
+    case rain = "Rain"
+    case thunderstorm = "Thunderstorm"
+    case snow = "Snow"
+    case mist = "Mist"
 
-    var title: Image {
+    var image: String {
         switch self {
-        case .clearSky: Image(systemName: "sun.max.fill")
-        case .fewClouds: Image(systemName: "cloud.fill")
-        case .scatteredClouds: Image(systemName: "cloud")
-        case .brokenClouds: Image(systemName: "cloud")
-        case .showerRain: Image(systemName: "cloud.heavyrain")
-        case .rain: Image(systemName: "cloud.rain")
-        case .thunderstorm: Image(systemName: "cloud.bolt.rain")
-        case .snow: Image(systemName: "snowflake")
-        case .mist: Image(systemName: "water.waves")
+        case .clearSky: "☀️"
+        case .clouds: "☁️"
+        case .rain: "🌧️"
+        case .thunderstorm: "⛈️"
+        case .snow: "❄️"
+        case .mist: "🌫️"
         }
     }
 }

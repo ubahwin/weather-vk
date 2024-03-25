6a79a322c93720ce2acf0a019c4090da50548d0e
Ivan Vdovin <ivan03_vdovin@mail.ru>
2024-03-25 21:43:53 +0300

    feat(weather-type): theme motion effect

--- WeatherVK/View/VKUI_SwiftUI/Component/Theme.swift
+++ WeatherVK/View/VKUI_SwiftUI/Component/Theme.swift
@@ -1,6 +1,10 @@
 import SwiftUI
+import CoreMotion
 
 struct Theme: View {
+    let motionManager = CMMotionManager()
+    @State private var offset = CGSize()
+
     let weatherType: WeatherType
     let date: Date
 
@@ -76,6 +80,30 @@ struct Theme: View {
             }
         }
         .frame(width: 0, height: 0)
+        .offset(offset)
+        .frame(width: 300, height: 150)
+        .clipShape(RoundedRectangle(cornerRadius: 25))
+        .shadow(radius: 10, y: 10)
+        .onAppear {
+            startMotionUpdates()
+        }
+    }
+
+    private func startMotionUpdates() {
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.deviceMotionUpdateInterval = 1.0 / 60.0
+            motionManager.startDeviceMotionUpdates(to: .main) { (data, _) in
+                guard let data = data else { return }
+                let attitude = data.attitude
+
+                let pitch = CGFloat(attitude.pitch)
+                let roll = CGFloat(attitude.roll)
+
+                self.offset = CGSize(width: roll * 8, height: pitch * 8)
+            }
+        } else {
+            print("Device motion is not available")
+        }
     }
 }
 
